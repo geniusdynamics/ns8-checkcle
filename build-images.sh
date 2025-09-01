c9a3438f094d768012db9b90478e9a61c30f190c
Kemboi Elvis <kemboielvis22@gmail.com>
2025-09-01 09:01:11 +0300

    refactor: replaced all kickstart names with checkcle

--- build-images.sh
+++ build-images.sh
@@ -18,17 +18,17 @@ reponame="checkcle"
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-checkcle container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-checkcle; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-checkcle -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-checkcle \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
